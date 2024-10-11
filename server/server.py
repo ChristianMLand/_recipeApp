a08362822a17be866a60b6f2fbf9b8062dee52ad
ChristianMLand <christianmland@outlook.com>
2024-10-11 16:01:38 -0700

    fix debug mode being set incorrectly

--- server/server.py
+++ server/server.py
@@ -5,6 +5,6 @@ from flask_app.controllers import recipe_controller, user_controller
 if __name__ == "__main__":
     app.run(
         port=os.getenv("PORT"), 
-        debug=os.getenv("MODE") != "dev",
+        debug=os.getenv("MODE") == "dev",
         # host='192.168.1.6'
     )
